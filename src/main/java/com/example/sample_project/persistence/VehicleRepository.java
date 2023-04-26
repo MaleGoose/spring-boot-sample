da47a29525f87d5d5652b5b5a2d440f11afd64de
MatteoStormblessed <matteozockt123@gmail.com>
2023-04-26 18:12:37 +0200

    Add to persitence layer
    - complex query with @Query annotation
    - tests

--- src/main/java/com/example/sample_project/persistence/VehicleRepository.java
+++ src/main/java/com/example/sample_project/persistence/VehicleRepository.java
@@ -1,11 +1,10 @@
 package com.example.sample_project.persistence;
 
-import com.example.sample_project.domain.Car;
-import com.example.sample_project.domain.MotorType;
-import com.example.sample_project.domain.Truck;
-import com.example.sample_project.domain.Vehicle;
+import com.example.sample_project.domain.*;
 import com.example.sample_project.persistence.projection.NameAndHorsepowerView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +17,12 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Car> findAllBy();
 
     List<NameAndHorsepowerView> findAllByPerson_Name_LastNameContainingAndMotorTypeOrderByHorsePowerDesc(String containedInLastName, MotorType motorType);
+
+    //Same Query but with SQL @Query Annotation
+    @Query("SELECT NEW  com.example.sample_project.persistence.projection.NameAndHorsepowerView(v.person.name, v.horsePower) " +
+            "FROM Vehicle v " +
+            "WHERE v.person.name.lastName LIKE %:containedInLastName% " +
+            "AND v.motorType = :motorType " +
+            "ORDER BY v.horsePower DESC")
+    List<NameAndHorsepowerView> nameAndHorsePowerView(@Param("containedInLastName")String containedInLastName, @Param("motorType") MotorType motorType);
 }
